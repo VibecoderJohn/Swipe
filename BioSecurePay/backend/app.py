fc01d83062c6c25bc7f81cd6f76349e1a96e1184
John Folorunsho Ajayi <johnfolorunshoajayi@gmail.com>
2025-09-03 16:21:42 +0100

    Update app.py

--- BioSecurePay/backend/app.py
+++ BioSecurePay/backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 from flask_pymongo import PyMongo
 from flask_jwt_extended import JWTManager
-from config import Config
+from .config import Config
 import sentry_sdk
 from sentry_sdk.integrations.flask import FlaskIntegration
 from routes import bp as api_bp
@@ -25,3 +25,4 @@ app.register_blueprint(api_bp, url_prefix='/api/v1')
 
 if __name__ == '__main__':
     app.run(debug=app.config['FLASK_ENV'] == 'development')
+
