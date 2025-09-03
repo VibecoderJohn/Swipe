4b204709ceb8a1552b843efa6dffe80644d47e61
John Folorunsho Ajayi <johnfolorunshoajayi@gmail.com>
2025-09-03 16:13:48 +0100

    Update config.py

--- BioSecurePay/backend/config.py
+++ BioSecurePay/backend/config.py
@@ -2,6 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'your-default-secret-key'
+    DEBUG = False
     MONGO_URI = os.environ.get('MONGO_URI') or 'mongodb://localhost:27017/biosecure_pay'
     SENTRY_DSN = os.environ.get('SENTRY_DSN')
     PAYSTACK_SECRET_KEY = os.environ.get('PAYSTACK_SECRET_KEY')
